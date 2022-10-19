eda8d4e6be62c0726c1d5cd651e68642b5f73f69
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-19 01:02:03 -0500

    chore: Indice component

--- src/components/Indice.js
+++ src/components/Indice.js
@@ -1,5 +1,6 @@
 // import styled from "@emotion/styled";
 import React from 'react'
+import styled from "@emotion/styled";
 import * as blogService from '../context/blogs-services'
 
 
@@ -17,19 +18,48 @@ export default function Indice() {
 
 
   return (
-      <table>
-        <tr>
-          <th>Título</th>
-          <th>Contenido</th>
-        </tr>
+      <Table>
+        <TableRowTitle>
+          <TableHeader>Título</TableHeader>
+          <TableHeader>Contenido</TableHeader>
+        </TableRowTitle>
         {blogs.map(x=>
         (
-          <tr>
-          <td><p>{x.title}</p></td>
-          <td><p>{x.body}</p></td>
-          </tr>
+          <TableRow>
+            <TableData>{x.title}</TableData>
+            <TableData>{x.body}</TableData>
+          </TableRow>
           ))}
-      </table>
+      </Table>
   )
 }
+
+const Table = styled.table`
+  padding: 32px;
+  width: 70%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+`;
+
+const TableRowTitle = styled.tr`
+  background-color: darkblue;
+  color: white;
+`;
+
+const TableRow = styled.tr`
+&:nth-child(even) {
+  background-color: lightblue;
+}
+`;
+
+const TableHeader= styled.th`
+border: 1px solid;
+padding: 16px;
+`
+const TableData= styled.td`
+border: 1px solid lightgray;
+padding: 16px;
+`
 
