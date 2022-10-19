bdc0ac6600c9e21f5851df3c5ab173c48c711093
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-19 14:05:17 -0500

    chore: add Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import Header from './components/Header'
+import { Route, Routes } from 'react-router-dom';
+
+// import Inicio from './components/Inicio'
+// <Inicio></Inicio>
+import Inicio from './components/Inicio'
+import Indice from './components/Indice'
+import Busqueda from './components/Busqueda'
+import About from './components/About'
+
 function App() {
   return (
-    <div>
-      <Header></Header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/indice" element={<Indice />} />
+        <Route path="/busqueda" element={<Busqueda />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+    </>
   );
 }
 
