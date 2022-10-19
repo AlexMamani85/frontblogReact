bc89d2eb81dcf4c00f68fa6ee34f3676f2ed8cf0
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-19 12:15:04 -0500

    chore: Inicio Component

---
+++ src/components/Inicio.js
@@ -0,0 +1,56 @@
+// import styled from "@emotion/styled";
+import React from 'react'
+import styled from "@emotion/styled";
+
+
+
+export default function Indice() {
+
+  return (
+      <>
+      <Wrapper>
+        <H1>
+        MÁS DE 200 SITIOS WEB Y BLOGS
+        </H1>
+        <Paragraph>
+          Contribuyendo de alta calidad investigado y escrito artículos como invitado a los blogs y sitios web autorizados y relevantes es una forma comprobada de aumentar el tráfico y aumentar el posicionamiento en los motores de búsqueda. Pero encontrar los sitios adecuados para publicar como invitados y a quién enviar su contenido puede ser complicado.
+        </Paragraph>
+        <Paragraph>
+          Aquí hay una lista de más de 200 blogs y sitios web de alta calidad en los que puedes publicar como invitado y contribuir con su contenido en 2022, lo que debería ayudar a impulsar la clasificación y el tráfico orgánico de su sitio.
+        </Paragraph>
+        <Paragraph>
+          A continuación, se incluye una lista ordenable y categorizada de blogs y sitios web de alta calidad que permiten publicaciones de invitados y para que usted envíe su contribución de publicación de invitado.
+        </Paragraph>
+        <Paragraph>
+          * Siempre recuerde leer y seguir las pautas de envío, y por supuesto no hay garantías de que ninguno de los sitios que se enumeran a continuación acepte su contribución al contenido, sin importar cuán alta sea la calidad de su publicación de invitado.
+        </Paragraph>
+        </Wrapper>
+
+      </>
+  );
+}
+
+const Wrapper = styled.div`
+  border: red 1px solid;
+  margin:16px;
+  padding: 16px;
+
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+
+  height:80%;
+  width:80%;
+
+`;
+
+
+const H1 = styled.h1`
+  margin: 16px;
+`;
+
+const Paragraph = styled.p`
+  padding: 16px;
+`;
