d42f931274c598aa3e2178394c0d401cc1fe4f05
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-21 17:26:26 -0500

    CHORE: getBlog-by-title Service  for front and back

--- src/context/blogs-services.js
+++ src/context/blogs-services.js
@@ -4,4 +4,9 @@ export async function getBlogs(){
   const data = await apiFetch("blogs")
 
   return data;
+};
+
+export async function getBlogByWORDS(words){
+  const data = await apiFetch("busqueda/"+words)
+  return data;
 }
