bfe46ac250ea082add7c5723885b31349bb14171
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-24 00:39:59 -0500

    CHORE: post new BLog

--- src/context/blogs-services.js
+++ src/context/blogs-services.js
@@ -9,4 +9,14 @@ export async function getBlogs(){
 export async function getBlogByWORDS(words){
   const data = await apiFetch("busqueda/"+words)
   return data;
+};
+
+export async function postNewBlog(body){
+  return await apiFetch("/blogs", { body: body }).then((u) => {
+    console.log(u);
+    const { result } = u;
+    // sessionStorage.setItem(tokenKey, token);
+    if (result) {return "Blog Created";}
+    else {return "Blog Not Created";}
+  }).catch((err)=>console.log(err));
 }
